b09ecd258c1c62afbcce243d50d0d5d70bb738d7
sbcar <sergio.barria@itwox.com>
2022-07-10 18:38:05 -0500

    remove old project

--- old/client/src/hooks/useSelector.ts
+++
@@ -1,4 +0,0 @@
-import { useSelector as _useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState } from '../redux';
-
-export const useSelector: TypedUseSelectorHook<RootState> = _useSelector;
--- old/client/src/redux/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './products.actions';
-export * from './cart.actions';
-export * from './user.actions';
