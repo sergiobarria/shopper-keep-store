7ca05c1316f16c051aa5bbf086ae4f2679b67ba3
sergiobarria <sbarria.dev@gmail.com>
2022-07-16 01:01:52 -0500

    chore(client): move products assets location

---
+++ packages/types/src/product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  id?: string | number;
+  _id?: string | number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
